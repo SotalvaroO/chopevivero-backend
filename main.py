3adb746eb089b416f9536d518944d795d5741224
Santiago Otalvaro Ospina <santiago.otalvaroo@udea.edu.co>
2022-03-01 23:08:29 -0500

    main.py added

---
+++ main.py
@@ -0,0 +1,11 @@
+
+from fastapi import FastAPI
+from starlette.responses import RedirectResponse
+
+app = FastAPI()
+
+
+@app.get('/')
+async def home():
+    res = RedirectResponse(url='/docs')
+    return res

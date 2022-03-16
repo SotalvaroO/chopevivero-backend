55a1d813d08dde67cc7207adb0d5ea1b4c9a388d
Santiago Otalvaro Ospina <santiago.otalvaroo@udea.edu.co>
2022-03-15 20:26:52 -0500

    Agregación de proyecto Java
    Agregación de proyecto Java

---
+++ src/main/java/co/edu/udea/chopeviverobackend/ChopeviveroBackendApplication.java
@@ -0,0 +1,13 @@
+package co.edu.udea.chopeviverobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChopeviveroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChopeviveroBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/udea/chopeviverobackend/ChopeviveroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.udea.chopeviverobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChopeviveroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
